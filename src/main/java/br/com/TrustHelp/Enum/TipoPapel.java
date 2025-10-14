03bc120af4315296717937588bb01e954f781b2e
Kayo Vinícius <104803911+kayo-benjamin@users.noreply.github.com>
2025-10-14 13:01:27 -0300

    Correção no TipoPapel (Enum) e Organizacao (Modelos)

--- src/main/java/br/com/TrustHelp/Enum/TipoPapel.java
+++ src/main/java/br/com/TrustHelp/Enum/TipoPapel.java
@@ -1,15 +1,7 @@
 package br.com.TrustHelp.Enum;
 
-import lombok.Getter;
-
-@Getter
 public enum TipoPapel {
-    ADMIN("admin"),
-    ANALISTA("analista"),
-    CLIENTE("cliente");
-
-    private final String tipoPapel;
-    TipoPapel(String tipoPapel) {
-        this.tipoPapel = tipoPapel;
-    }
+    ADMIN,
+    ANALISTA,
+    CLIENTE;
 }
--- src/main/java/br/com/TrustHelp/Model/Organizacao.java
+++ src/main/java/br/com/TrustHelp/Model/Organizacao.java
@@ -1,11 +1,7 @@
 package br.com.TrustHelp.Model;
 
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Builder
 public class Organizacao {
     private int id;
