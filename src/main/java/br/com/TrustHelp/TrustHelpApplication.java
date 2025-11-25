efaa84b5b8483c53440b63318e51b05ffa35478b
Kayo Vinícius <104803911+kayo-benjamin@users.noreply.github.com>
2025-11-25 17:53:35 -0300

    atualizando codigo

--- src/main/java/br/com/TrustHelp/TrustHelpApplication.java
+++ src/main/java/br/com/TrustHelp/TrustHelpApplication.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
 
 @SpringBootApplication()
 public class TrustHelpApplication implements CommandLineRunner {
